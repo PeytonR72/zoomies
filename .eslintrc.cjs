29959c4dc8220b33fa3be5d56f8daa09eedaabd0
PeytonR72 <peytonr7272@gmail.com>
2026-06-04 21:55:29 -0700

    chore: root monorepo scaffold (workspaces, ts, lint, format)

---
+++ .eslintrc.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  env: { browser: true, es2022: true, node: true },
+  ignorePatterns: ['dist', 'node_modules', '*.config.ts', '*.config.cjs'],
+  rules: { '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }] },
+};
